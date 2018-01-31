05cc69c1c3a9276714fbaa72402f7875a9f888e2
Justin Worsdale <whayler1@gmail.com>
2018-01-31 18:36:12 -0500

    event data in timeline page

--- src/containers/Timeline/Timeline.js
+++ src/containers/Timeline/Timeline.js
@@ -3,18 +3,35 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { Actions } from 'react-native-router-flux';
 
+import { getUserEvents as getUserEventsAction } from '../../redux/userEvents/userEvents.actions';
+
 import Template from './Timeline.template';
 
 class Timeline extends Component {
-  static propTypes = {}
+  static propTypes = {
+    userEvents: PropTypes.array,
+    getUserEvents: PropTypes.func.isRequired,
+  };
 
   goToDashboard = () => Actions.dashboard();
 
+  componentWillMount() {
+    this.props.getUserEvents(100, 0);
+  }
+
   render() {
     return <Template
+      userEvents={this.props.userEvents}
       goToDashboard={this.goToDashboard}
     />;
   }
 }
 
-export default connect()(Timeline);
+export default connect(
+  state => ({
+    userEvents: state.userEvents.rows,
+  }),
+  {
+    getUserEvents: getUserEventsAction
+  }
+)(Timeline);
