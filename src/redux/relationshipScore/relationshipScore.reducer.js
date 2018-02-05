9802b0e0f2b42583b16a281730ba1d4ab31e566f
Justin Worsdale <whayler1@gmail.com>
2018-02-05 17:44:08 -0500

    draw eye with ART api

--- src/redux/relationshipScore/relationshipScore.reducer.js
+++ src/redux/relationshipScore/relationshipScore.reducer.js
@@ -7,6 +7,12 @@ const defaultState = {
   id: '',
   createdAt: '',
   score: undefined,
+  /**
+   * JW: scoreQuartile isn't in the model. It's an extrapolation that tells us
+   * what quartile the relationshipScore is in. i.e. 0-24 = 0, 25-49 = 1...
+   * this helps simplify drawing views that only care about quartile.
+   */
+  scoreQuartile: 0,
 };
 
 export default function reducer(state = defaultState, action) {
@@ -15,6 +21,7 @@ export default function reducer(state = defaultState, action) {
       return {
         ...state,
         ..._.pick(action, ['id', 'createdAt', 'score']),
+        scoreQuartile: Math.min(Math.floor(action.score * 0.04), 3),
       }
     default:
       return state;
