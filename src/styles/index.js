3f178df5e91b956b46004fdc5eccca97b77c0905
Justin Worsdale <whayler1@gmail.com>
2018-02-21 17:01:05 -0500

    error well styles; add error well for server errors on login

--- src/styles/index.js
+++ src/styles/index.js
@@ -3,11 +3,13 @@ import forms from './forms.styles';
 import scene from './scene.styles';
 import vars from './vars';
 import modal from './modal.styles';
+import wells from './wells.styles';
 
 export {
   buttons,
   forms,
   scene,
   modal,
+  wells,
   vars,
 }
