3c8645b4cd07141ab711977a892cd19be329e9bd
Justin Worsdale <whayler1@gmail.com>
2018-03-21 20:41:20 -0400

    app segment makes identify call

--- src/config.js
+++ src/config.js
@@ -1,9 +1,11 @@
 const baseUrl = process.env.BASE_URL || 'http://localhost:3000';
 // const baseUrl = process.env.BASE_URL || 'https://luvup.io';
+const segmentWriteKey = process.env.SEGMENT_WRITE_KEY || '';
 
 const config = {
   baseUrl,
   graphQlUrl: `${baseUrl}/graphql`,
+  segmentWriteKey,
   maxItemsPerHour: 5,
   swipeThreshold: 40,
 }
