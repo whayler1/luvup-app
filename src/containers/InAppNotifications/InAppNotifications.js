2e88839adc13ca5189a7fde2d658d16e7aa48be1
Justin Worsdale <whayler1@gmail.com>
2018-05-10 21:31:57 -0400

    stub notifications service

--- src/containers/InAppNotifications/InAppNotifications.js
+++ src/containers/InAppNotifications/InAppNotifications.js
@@ -1,11 +1,10 @@
 import React, { PureComponent } from 'react';
-import { Vibration } from 'react-native';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { Notifications } from 'expo';
+
 import Template from './InAppNotifications.template';
 
-import _ from 'lodash';
+
 
 class InAppNotifications extends PureComponent {
   state = {
@@ -16,27 +15,11 @@ class InAppNotifications extends PureComponent {
 
   componentDidMount() {
 
-    Notifications.addListener(this.onNotificationReceived);
-
-  }
-
-  onNotificationReceived = notification => {
-    const updatedNotifications = [...this.state.notifications, notification];
-    const luvupReceivedNotifications = updatedNotifications.filter(notification => _.get(notification, 'data.type') === 'luvup-received');
-    const jalapenoReceivedNotifications = updatedNotifications.filter(notification => _.get(notification, 'data.type') === 'jalapeno-received');
-
-    this.setState({
-      notifications: updatedNotifications,
-      luvupReceivedNotifications,
-      jalapenoReceivedNotifications
-    });
-    Vibration.vibrate();
   }
 
   render() {
     return (
       <Template
-        children={this.props.children}
         isReady={this.props.isFontLoaded}
       />
     )
