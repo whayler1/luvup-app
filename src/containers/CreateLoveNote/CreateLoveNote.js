5c22dd90b08f270f67a6686643ea74a4a7448bc3
Justin Worsdale <whayler1@gmail.com>
2018-06-18 20:53:28 -0400

    z

--- src/containers/CreateLoveNote/CreateLoveNote.js
+++ src/containers/CreateLoveNote/CreateLoveNote.js
@@ -1,9 +1,16 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import _ from 'lodash';
 
+import { createLoveNote as createLoveNoteAction } from '../../redux/loveNote/loveNote.actions';
 import Template from './CreateLoveNote.template';
 
 class CreateLoveNote extends PureComponent {
+  static propTypes = {
+    createLoveNote: PropTypes.func.isRequired,
+  }
+
   constructor(props) {
     super(props);
     this.state = {
@@ -13,11 +20,32 @@ class CreateLoveNote extends PureComponent {
     };
   }
 
+  onNoteChange = note => this.setState({ note });
+  addLuvup = () => this.setState({ numLuvups: this.state.numLuvups + 1 });
+  removeLuvup = () => this.setState({ numLuvups: this.state.numLuvups - 1 });
+  addJalapeno = () => this.setState({ numLuvups: this.state.numJalapenos + 1 });
+  removeJalapeno = () => this.setState({ numLuvups: this.state.numJalapenos - 1 });
+
   render() {
+    const props = {
+      ...this.state,
+      ..._.pick(this, [
+        'onNoteChange',
+        'addLuvup',
+        'removeLuvup',
+        'addJalapeno',
+        'removeJalapeno',
+      ]),
+    }
     return (
-      <Template {...this.state} />
+      <Template {...props} />
     );
   }
 }
 
-export default connect()(CreateLoveNote);
+export default connect(
+  null,
+  {
+    createLoveNote: createLoveNoteAction,
+  },
+)(CreateLoveNote);
