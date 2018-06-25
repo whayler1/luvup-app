afcc5a525d3e1eff9879c52985f76e74c7b0f716
Justin Worsdale <whayler1@gmail.com>
2018-06-24 21:24:25 -0400

    move limit exceeded modal to its own component

--- src/containers/Dashboard/Dashboard.template.js
+++ src/containers/Dashboard/Dashboard.template.js
@@ -20,6 +20,7 @@ import CoinArt from '../../components/CoinArt';
 import JalapenoArt from '../../components/JalapenoArt';
 import LoveNoteArt from '../../components/LoveNoteArt';
 import Hero from '../Hero';
+import LimitExceededModal from '../../components/LimitExceededModal';
 
 export default ({
   userFirstName,
@@ -81,47 +82,13 @@ export default ({
         <LoveNoteArt />
       </TouchableOpacity>
     </View>
-    <Modal
-      visible={isModalOpen}
-      animationType={'fade'}
-      onRequestClose={closeModal}
-      transparent={true}
-    >
-      <View style={modal.outerContainer}>
-        <View style={modal.innerContainer}>
-          <View>
-            {modalContent === 'coin' && <CoinArt
-              recentlySentCoinCount={config.maxItemsPerHour}
-            />}
-            {modalContent === 'jalapeno' && <JalapenoArt
-              recentlySentJalapenoCount={config.maxItemsPerHour}
-            />}
-          </View>
-          <View style={{
-            marginTop: 16,
-            alignItems: 'center',
-          }}>
-            <Text style={modal.title}>
-              {modalContent === 'coin' && 'Hourly Luvup\nLimit Exceeded'}
-              {modalContent === 'jalapeno' && 'Hourly Jalapeno\nLimit Exceeded'}
-            </Text>
-            <Text style={modal.copy}>
-              {modalContent === 'coin' && `Wow! ${loverFirstName} must be on fire right now. You've sent the max hourly limit of ${config.maxItemsPerHour} luvups. You'll have the opportunity to send another luvup ${coinsAvailableTime}.`}
-              {modalContent === 'jalapeno' && `Looks like things are getting spicy with ${loverFirstName} right now. You've sent the max hourly limit of ${config.maxItemsPerHour} jalapenos. You'll have the oportunity to send another jalapeno ${jalapenosAvailableTime}.`}
-            </Text>
-          </View>
-          <View style={modal.buttonContainer}>
-            <Button
-              raised
-              onPress={closeModal}
-              containerViewStyle={buttons.infoContainer}
-              buttonStyle={buttons.infoButton}
-              textStyle={buttons.infoText}
-              title='Dismiss'
-            />
-          </View>
-        </View>
-      </View>
-    </Modal>
+    <LimitExceededModal
+      isModalOpen={isModalOpen}
+      closeModal={closeModal}
+      modalContent={modalContent}
+      loverFirstName={loverFirstName}
+      coinsAvailableTime={coinsAvailableTime}
+      jalapenosAvailableTime={jalapenosAvailableTime}
+    />
   </View>
 );
