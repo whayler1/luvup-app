c14b1852de230581bb59b3a421ca161da1f63398
Justin Worsdale <whayler1@gmail.com>
2018-06-26 19:41:38 -0400

    add get love notes action

--- src/redux/loveNote/loveNote.actions.js
+++ src/redux/loveNote/loveNote.actions.js
@@ -3,6 +3,9 @@ import _ from 'lodash';
 import config from '../../config';
 
 export const CREATE_LOVE_NOTE_SUCCESS = 'love-note/create-love-note-success';
+export const GET_RECEIVED_LOVE_NOTES_ATTEMPT = 'love-note/get-received-love-notes-attempt';
+export const GET_RECEIVED_LOVE_NOTES_SUCCESS = 'love-note/get-received-love-notes-success';
+export const GET_RECEIVED_LOVE_NOTES_FAILURE = 'love-note/get-received-love-notes-failure';
 
 export const createLoveNote = (note, { numLuvups = 0, numJalapenos = 0 }) => async dispatch => {
   try {
@@ -41,5 +44,46 @@ export const createLoveNote = (note, { numLuvups = 0, numJalapenos = 0 }) => asy
   } catch (err) {
     console.log('\n\nerr', err);
     return err;
+  }
+};
+
+export const getReceivedLoveNotes = ({ isRead, limit = 10, offset = 0 }) => async dispatch => {
+  dispatch({ type: GET_RECEIVED_LOVE_NOTES_ATTEMPT });
+  try {
+    const isReadArg = _.isBoolean(isRead) ? `isRead: ${isRead}` : '';
+    const res = await superagent.post(config.graphQlUrl, {
+      query: `{
+        receivedLoveNotes(
+          limit: ${limit}
+          offset: ${offset}
+          ${isReadArg}
+        ) {
+      		count
+          rows {
+            id note createdAt numLuvups numJalapenos
+          }
+        }
+      }`,
+    });
+
+    const { count, rows } = _.get(res, 'body.data.receivedLoveNotes', {});
+
+    if (res.ok && _.isNumber(count)) {
+      dispatch({
+        type: GET_RECEIVED_LOVE_NOTES_SUCCESS,
+        count,
+        rows,
+      });
+    } else {
+      dispatch({
+        type: GET_RECEIVED_LOVE_NOTES_FAILURE,
+        error: 'response error',
+      });
+    }
+  } catch (error) {
+    dispatch({
+      type: GET_RECEIVED_LOVE_NOTES_FAILURE,
+      error,
+    });
   }
 };
