82ce3744a144c8d11b713851a974866f0cd37633
Justin Worsdale <whayler1@gmail.com>
2018-06-26 19:48:23 -0400

    get received love notes in reducer

--- src/redux/loveNote/loveNote.reducer.js
+++ src/redux/loveNote/loveNote.reducer.js
@@ -1,13 +1,39 @@
 import _ from 'lodash';
 import {
-  CREATE_LOVE_NOTE_SUCCESS,
+  // CREATE_LOVE_NOTE_SUCCESS,
+  GET_RECEIVED_LOVE_NOTES_ATTEMPT,
+  GET_RECEIVED_LOVE_NOTES_SUCCESS,
+  GET_RECEIVED_LOVE_NOTES_FAILURE,
 } from './loveNote.reducer';
 
-const defaultState = {};
+const defaultState = {
+  isGetReceivedLoveNotesInFlight: false,
+  getReceivedLoveNotesError: '',
+  receivedLoveNoteCount: null,
+  receivedLoveNotes: [],
+};
 
 export default function reducer(state = defaultState, action) {
   switch(action.type) {
-    case CREATE_LOVE_NOTE_SUCCESS:
+    case GET_RECEIVED_LOVE_NOTES_ATTEMPT:
+      return {
+        ...state,
+        isGetReceivedLoveNotesInFlight: true,
+        getReceivedLoveNotesError: '',
+      };
+    case GET_RECEIVED_LOVE_NOTES_SUCCESS:
+      return {
+        ...state,
+        isGetReceivedLoveNotesInFlight: false,
+        receivedLoveNoteCount: action.count,
+        receivedLoveNotes: action.rows,
+      };
+    case GET_RECEIVED_LOVE_NOTES_FAILURE:
+      return {
+        ...state,
+        isGetReceivedLoveNotesInFlight: false,
+        getReceivedLoveNotesError: action.error,
+      };
     default:
       return state;
   }
