eb9deca79669f633b49bee09954e274c7f73c572
Justin Worsdale <whayler1@gmail.com>
2018-06-25 21:15:12 -0400

    coin reducer handles love note success

--- src/redux/loveNote/loveNote.reducer.js
+++ src/redux/loveNote/loveNote.reducer.js
@@ -1,13 +1,13 @@
 import _ from 'lodash';
 import {
-  CREATE_LOVE_NOTE,
+  CREATE_LOVE_NOTE_SUCCESS,
 } from './loveNote.reducer';
 
 const defaultState = {};
 
 export default function reducer(state = defaultState, action) {
   switch(action.type) {
-    case CREATE_LOVE_NOTE:
+    case CREATE_LOVE_NOTE_SUCCESS:
     default:
       return state;
   }
