668f28d08905a7a69106c4785eb71005ad34f326
Justin Worsdale <whayler1@gmail.com>
2018-07-04 13:02:17 -0400

    create app state listener service

---
+++ src/services/appStateListener.js
@@ -0,0 +1,19 @@
+import { AppState } from 'react-native';
+
+let appState = AppState.currentState;
+
+const handleAppStateChange = (nextAppState) => {
+  if (appState.match(/inactive|background/) && nextAppState === 'active') {
+    console.log('App has come to the foreground!')
+    appState = nextAppState;
+    
+  }
+};
+
+const start = () => {
+  AppState.addEventListener('change', handleAppStateChange);
+};
+
+const stop = () => {
+  AppState.removeEventListener('change', handleAppStateChange);
+};
