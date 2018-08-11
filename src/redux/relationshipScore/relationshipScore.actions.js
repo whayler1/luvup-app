db1c44a5714cb3e84111da87f2ae7c8eb531662d
Justin Worsdale <whayler1@gmail.com>
2018-08-11 18:27:50 -0400

    get relationship scores reducer

--- src/redux/relationshipScore/relationshipScore.actions.js
+++ src/redux/relationshipScore/relationshipScore.actions.js
@@ -1,10 +1,17 @@
 import _ from 'lodash';
 import superagent from 'superagent';
+import graphQlRequest from '../../helpers/graphQlRequest';
 
 import config from '../../config';
 
 export const CREATE_RELATIONSHIP_SCORE =
   'relationship-score/create-relationship-score';
+export const GET_RELATIONSHIP_SCORES_ATTEMPT =
+  'relationship-score/get-relationship-scores-attempt';
+export const GET_RELATIONSHIP_SCORES_SUCCESS =
+  'relationship-score/get-relationship-scores-success';
+export const GET_RELATIONSHIP_SCORES_FAILURE =
+  'relationship-score/get-relationship-scores-failure';
 
 export const createRelationshipScore = () => async dispatch => {
   try {
@@ -32,5 +39,46 @@ export const createRelationshipScore = () => async dispatch => {
     return res;
   } catch (err) {
     return err;
+  }
+};
+
+export const getRelationshipScores = (
+  limit = 20,
+  offset = 0
+) => async dispatch => {
+  dispatch({ type: GET_RELATIONSHIP_SCORES_ATTEMPT });
+
+  try {
+    const res = await graphQlRequest(`{
+      relationshipScores(
+        limit: "${limit}"
+        offset: "${offset}"
+      ) {
+        rows {
+          id
+          score
+          createdAt
+        }
+      }
+    }`);
+
+    const { rows } = res;
+
+    if (_.isArray(rows)) {
+      dispatch({
+        type: GET_RELATIONSHIP_SCORES_SUCCESS,
+        rows,
+      });
+    } else {
+      dispatch({
+        type: GET_RELATIONSHIP_SCORES_FAILURE,
+        error: 'response error',
+      });
+    }
+  } catch (error) {
+    dispatch({
+      type: GET_RELATIONSHIP_SCORES_FAILURE,
+      error,
+    });
   }
 };
