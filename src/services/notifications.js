69b255ab920a625a9432b46b868cf0fcc956a437
Justin Worsdale <whayler1@gmail.com>
2018-08-13 20:54:27 -0400

    get relationship score when token received

--- src/services/notifications.js
+++ src/services/notifications.js
@@ -2,6 +2,7 @@ import { Notifications } from 'expo';
 import { Vibration } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import { addNotification } from '../redux/notifications/notifications.actions';
+import { getRelationshipScore } from '../redux/relationshipScore/relationshipScore.actions';
 import _ from 'lodash';
 import store from '../redux';
 
@@ -9,6 +10,7 @@ let eventSubscription;
 
 export const onNotificationReceived = notification => {
   store.dispatch(addNotification(notification));
+  store.dispatch(getRelationshipScore());
   Actions.notificationLightbox();
   Vibration.vibrate();
 };
