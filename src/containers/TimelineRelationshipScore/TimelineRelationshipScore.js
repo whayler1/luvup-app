4afba0da0c2844bfdb9a97ff39b02350299dd3d6
Justin Worsdale <whayler1@gmail.com>
2018-08-21 20:13:28 -0400

    convert rs score to flatlist

--- src/containers/TimelineRelationshipScore/TimelineRelationshipScore.js
+++ src/containers/TimelineRelationshipScore/TimelineRelationshipScore.js
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
-import { Text, ScrollView, View } from 'react-native';
+import { Text, FlatList, View } from 'react-native';
 import PropTypes from 'prop-types';
 import moment from 'moment';
 import { Surface } from 'ReactNativeART';
@@ -33,6 +33,58 @@ const getFill = score => {
   return Color(lowerColor).mix(Color(higherColor), pct);
 };
 
+const renderItem = ({ item }) => (
+  <View
+    key={item.id}
+    style={{
+      borderRightColor: vars.blueGrey50,
+      borderRightWidth: 1,
+      paddingLeft: 16,
+      paddingRight: 16,
+      alignItems: 'center',
+    }}>
+    <Text
+      style={{
+        fontFamily: vars.fontBlack,
+        color: vars.blueGrey500,
+        fontSize: 12,
+      }}>
+      {moment(item.day).format("MMM D, 'YY")}
+    </Text>
+    <View
+      style={{
+        flex: 1,
+        width: 70,
+      }}>
+      <View
+        style={{
+          position: 'absolute',
+          bottom: `${item.score}%`,
+          left: 0,
+          right: 0,
+          width: 70,
+          height: 70,
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <Surface width={70} height={70} style={{ position: 'absolute' }}>
+          <Circle radius={35} fill={getFill(item.score)} />
+        </Surface>
+        <Text
+          style={{
+            fontFamily: vars.fontBlack,
+            color: 'white',
+            fontSize: 18,
+          }}>
+          {item.score}
+        </Text>
+      </View>
+    </View>
+  </View>
+);
+
+const keyExtractor = item => item.id;
+
 class TimelineRelationshipScore extends PureComponent {
   static onEnter() {
     store.dispatch(getRelationshipScores(100));
@@ -51,60 +103,13 @@ class TimelineRelationshipScore extends PureComponent {
 
   render() {
     return (
-      <ScrollView horizontal style={{ marginTop: 90 }}>
-        {this.props.relationshipScoresByDate.map(score => (
-          <View
-            key={score.id}
-            style={{
-              borderRightColor: vars.blueGrey50,
-              borderRightWidth: 1,
-              paddingLeft: 16,
-              paddingRight: 16,
-              alignItems: 'center',
-            }}>
-            <Text
-              style={{
-                fontFamily: vars.fontBlack,
-                color: vars.blueGrey500,
-                fontSize: 12,
-              }}>
-              {moment(score.day).format("MMM D, 'YY")}
-            </Text>
-            <View
-              style={{
-                flex: 1,
-                width: 70,
-              }}>
-              <View
-                style={{
-                  position: 'absolute',
-                  bottom: `${score.score}%`,
-                  left: 0,
-                  right: 0,
-                  width: 70,
-                  height: 70,
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                }}>
-                <Surface
-                  width={70}
-                  height={70}
-                  style={{ position: 'absolute' }}>
-                  <Circle radius={35} fill={getFill(score.score)} />
-                </Surface>
-                <Text
-                  style={{
-                    fontFamily: vars.fontBlack,
-                    color: 'white',
-                    fontSize: 18,
-                  }}>
-                  {score.score}
-                </Text>
-              </View>
-            </View>
-          </View>
-        ))}
-      </ScrollView>
+      <FlatList
+        horizontal
+        style={{ marginTop: 90 }}
+        renderItem={renderItem}
+        data={this.props.relationshipScoresByDate}
+        keyExtractor={keyExtractor}
+      />
     );
   }
 }
