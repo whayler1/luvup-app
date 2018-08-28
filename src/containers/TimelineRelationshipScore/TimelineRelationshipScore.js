b4c262006193c694513a41df4d2f820787a571fd
Justin Worsdale <whayler1@gmail.com>
2018-08-27 21:36:13 -0400

    rs timeline flatlist gets more scores on end reached

--- src/containers/TimelineRelationshipScore/TimelineRelationshipScore.js
+++ src/containers/TimelineRelationshipScore/TimelineRelationshipScore.js
@@ -5,6 +5,7 @@ import PropTypes from 'prop-types';
 import moment from 'moment';
 import { Surface } from 'ReactNativeART';
 import Color from 'color';
+import _ from 'lodash';
 
 import { getRelationshipScores } from '../../redux/relationshipScore/relationshipScore.actions';
 import { store } from '../../redux';
@@ -38,8 +39,6 @@ const renderItem = ({ item }) => (
   <View
     key={item.id}
     style={{
-      borderRightColor: vars.blueGrey50,
-      borderRightWidth: 1,
       paddingLeft: 16,
       paddingRight: 16,
       alignItems: 'center',
@@ -84,11 +83,23 @@ const renderItem = ({ item }) => (
   </View>
 );
 
+const ItemSeparatorComponent = () => (
+  <View
+    style={{
+      borderRightColor: vars.blueGrey50,
+      borderRightWidth: 1,
+    }}
+  />
+);
+
 const keyExtractor = item => item.id;
+
+const RELATIONSHIP_SCORE_LIMIT = 100;
+let relationshipScoreOffset = 0;
 
 class TimelineRelationshipScore extends PureComponent {
   static onEnter() {
-    store.dispatch(getRelationshipScores(100));
+    store.dispatch(getRelationshipScores(RELATIONSHIP_SCORE_LIMIT));
   }
 
   static propTypes = {
@@ -100,23 +111,48 @@ class TimelineRelationshipScore extends PureComponent {
         day: PropTypes.string.isRequired,
       })
     ),
-    isGettingRelationshipScores: PropTypes.bool.isRequired,
+    relationshipScores: PropTypes.array,
+    relationshipScoreCount: PropTypes.number,
+    isGettingRelationshipScores: PropTypes.bool,
     getRelationshipScoresError: PropTypes.string,
+    getRelationshipScores: PropTypes.func.isRequired,
+  };
+
+  handleEndReached = () => {
+    const { relationshipScores, relationshipScoreCount } = this.props;
+
+    if (
+      _.isArray(relationshipScores) &&
+      relationshipScores.length < relationshipScoreCount
+    ) {
+      relationshipScoreOffset += RELATIONSHIP_SCORE_LIMIT;
+      this.props.getRelationshipScores(
+        RELATIONSHIP_SCORE_LIMIT,
+        relationshipScoreOffset
+      );
+    }
   };
 
   render() {
-    const { isGettingRelationshipScores } = this.props;
+    const {
+      isGettingRelationshipScores,
+      relationshipScoresByDate,
+    } = this.props;
     const style = { marginTop: 90 };
     if (isGettingRelationshipScores) {
       style.alignSelf = 'stretch';
     }
     return (
       <FlatList
-        horizontal={!isGettingRelationshipScores}
+        horizontal={
+          !isGettingRelationshipScores || relationshipScoresByDate.length > 0
+        }
         style={style}
         renderItem={renderItem}
-        data={this.props.relationshipScoresByDate}
+        data={relationshipScoresByDate}
         keyExtractor={keyExtractor}
+        onEndReached={this.handleEndReached}
+        ItemSeparatorComponent={ItemSeparatorComponent}
         ListEmptyComponent={
           <ListEmptyComponent
             isInFlight={isGettingRelationshipScores}
@@ -128,10 +164,17 @@ class TimelineRelationshipScore extends PureComponent {
   }
 }
 
-export default connect(state => ({
-  relationshipScoresByDate: state.relationshipScore.relationshipScoresByDate,
-  isGettingRelationshipScores:
-    state.relationshipScore.isGettingRelationshipScores,
-  getRelationshipScoresError:
-    state.relationshipScore.getRelationshipScoresError,
-}))(TimelineRelationshipScore);
+export default connect(
+  state => ({
+    relationshipScoresByDate: state.relationshipScore.relationshipScoresByDate,
+    relationshipScoreCount: state.relationshipScore.count,
+    relationshipScores: state.relationshipScore.relationshipScores,
+    isGettingRelationshipScores:
+      state.relationshipScore.isGettingRelationshipScores,
+    getRelationshipScoresError:
+      state.relationshipScore.getRelationshipScoresError,
+  }),
+  {
+    getRelationshipScores,
+  }
+)(TimelineRelationshipScore);
