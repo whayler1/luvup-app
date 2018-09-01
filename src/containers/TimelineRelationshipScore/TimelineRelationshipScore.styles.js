d01738341e90f5133b7fb093abfd1b801f06bb3c
Justin Worsdale <whayler1@gmail.com>
2018-09-01 19:39:13 -0400

    rs timeline stylesheet

---
+++ src/containers/TimelineRelationshipScore/TimelineRelationshipScore.styles.js
@@ -0,0 +1,35 @@
+import { StyleSheet } from 'react-native';
+
+import { vars } from '../../styles';
+
+export default StyleSheet.create({
+  renderItemContainer: {
+    paddingLeft: 16,
+    paddingRight: 16,
+    alignItems: 'center',
+  },
+  renderItemText: {
+    fontFamily: vars.fontBlack,
+    color: vars.blueGrey500,
+    fontSize: 12,
+  },
+  renderItemScoreContainer: {
+    flex: 1,
+    width: 70,
+  },
+  renderItemScore: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    width: 70,
+    height: 70,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  renderItemBubble: { position: 'absolute' },
+  renderItemScoreText: {
+    fontFamily: vars.fontBlack,
+    color: 'white',
+    fontSize: 18,
+  },
+});
