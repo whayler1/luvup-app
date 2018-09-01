2f404156c8d4395c42b7f589fbd307c7a846bc41
Justin Worsdale <whayler1@gmail.com>
2018-09-01 19:06:29 -0400

    remove unneeded functions

--- src/containers/TimelineRelationshipScore/TimelineRelationshipScore.js
+++ src/containers/TimelineRelationshipScore/TimelineRelationshipScore.js
@@ -1,87 +1,14 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
-import { Text, FlatList, View } from 'react-native';
+import { FlatList, View } from 'react-native';
 import PropTypes from 'prop-types';
 import moment from 'moment';
-import { Surface } from 'ReactNativeART';
-import Color from 'color';
 
 import { getRelationshipScoresByDay } from '../../redux/relationshipScore/relationshipScore.actions';
 import { store } from '../../redux';
 import { vars } from '../../styles';
-import Circle from '../../components/Circle';
 import ListEmptyComponent from '../Timeline/Timeline.ListEmptyComponent.template';
 import TimelineRelationshipScoreRenderItem from './TimelineRelationshipScoreRenderItem';
-
-const getFill = score => {
-  let lowerColor;
-  let higherColor;
-  let threshold;
-  if (score < 33) {
-    lowerColor = vars.blue500;
-    higherColor = vars.purple500;
-    threshold = 0;
-  } else if (score < 66) {
-    lowerColor = vars.purple500;
-    higherColor = vars.red500;
-    threshold = 33;
-  } else {
-    lowerColor = vars.red500;
-    higherColor = vars.pink500;
-    threshold = 66;
-  }
-
-  const pct = (score - threshold) / 33;
-  return Color(lowerColor).mix(Color(higherColor), pct);
-};
-
-const renderItem = ({ item }) => (
-  <View
-    key={item.relationshipScore.id}
-    style={{
-      paddingLeft: 16,
-      paddingRight: 16,
-      alignItems: 'center',
-    }}>
-    <Text
-      style={{
-        fontFamily: vars.fontBlack,
-        color: vars.blueGrey500,
-        fontSize: 12,
-      }}>
-      {moment(item.day).format("MMM D, 'YY")}
-    </Text>
-    <View
-      style={{
-        flex: 1,
-        width: 70,
-      }}>
-      <View
-        style={{
-          position: 'absolute',
-          bottom: `${item.relationshipScore.score}%`,
-          left: 0,
-          right: 0,
-          width: 70,
-          height: 70,
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}>
-        <Surface width={70} height={70} style={{ position: 'absolute' }}>
-          <Circle radius={35} fill={getFill(item.relationshipScore.score)} />
-        </Surface>
-        <Text
-          style={{
-            fontFamily: vars.fontBlack,
-            color: 'white',
-            fontSize: 18,
-          }}>
-          {item.relationshipScore.score}
-        </Text>
-      </View>
-    </View>
-  </View>
-);
 
 const ItemSeparatorComponent = () => (
   <View
