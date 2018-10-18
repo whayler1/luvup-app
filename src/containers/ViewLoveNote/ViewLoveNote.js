a1b92f5eadf46b636e34417938ff0f1548b236e7
Justin Worsdale <whayler1@gmail.com>
2018-10-17 21:44:03 -0400

    get love note data in view love note view

--- src/containers/ViewLoveNote/ViewLoveNote.js
+++ src/containers/ViewLoveNote/ViewLoveNote.js
@@ -6,22 +6,49 @@ import PropTypes from 'prop-types';
 class ViewLoveNote extends PureComponent {
   static propTypes = {
     loveNoteId: PropTypes.string.isRequired,
+    userId: PropTypes.string.isRequired,
+    loverFirstName: PropTypes.string.isRequired,
+    loveNotes: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        note: PropTypes.string.isRequired,
+      })
+    ),
   };
 
-  handleMeowPress() {
-    console.log('meow press ', this.props.loveNoteId);
+  constructor(props) {
+    super(props);
+    const { loveNotes, userId } = props;
+
+    this.loveNote = loveNotes.find(
+      loveNote => loveNote.id === props.loveNoteId
+    );
+    this.isSender = userId === this.loveNote.senderId;
   }
 
   render() {
     return (
       <View>
         <Text>Love Note Id: {this.props.loveNoteId}</Text>
-        <TouchableOpacity onPress={this.handleMeowPress}>
-          Meowowow
-        </TouchableOpacity>
+        {this.isSender ? (
+          <Text>
+            You sent {this.props.loverFirstName} a love note on{' '}
+            {this.loveNote.createdAt}.
+          </Text>
+        ) : (
+          <Text>
+            {this.props.loverFirstName} sent you a love note on{' '}
+            {this.loveNote.createdAt}.
+          </Text>
+        )}
+        <Text>{decodeURI(this.loveNote.note)}</Text>
       </View>
     );
   }
 }
 
-export default connect(state => ({}))(ViewLoveNote);
+export default connect(state => ({
+  loveNotes: state.loveNote.loveNotes,
+  userId: state.user.id,
+  loverFirstName: state.lover.firstName,
+}))(ViewLoveNote);
