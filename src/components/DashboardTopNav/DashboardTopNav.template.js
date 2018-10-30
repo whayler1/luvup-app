a23790e8e63feff4bf9a5ef8623c6e78ac92733c
Justin Worsdale <whayler1@gmail.com>
2018-10-29 20:59:51 -0400

    get unread love notes on set love notes read success

--- src/components/DashboardTopNav/DashboardTopNav.template.js
+++ src/components/DashboardTopNav/DashboardTopNav.template.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, Image } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import _ from 'lodash';
 
 import Pushdown from '../../components/Pushdown';
 import NotificationDot from '../../components/NotificationDot';
 import CoinArt from '../../components/CoinArt';
 import { vars } from '../../styles';
+import styles from './DashboardTopNav.styles';
 
 export default ({
   coinCount,
@@ -24,38 +25,18 @@ export default ({
   unreadReceivedLoveNoteCount,
   relationshipScore,
 }) => (
-  <View
-    style={{
-      position: 'absolute',
-      alignItems: 'center',
-      top: 0,
-      left: 0,
-      right: 0,
-      paddingTop: 16,
-      marginTop: 28,
-      zIndex: 10,
-    }}>
+  <View style={styles.container}>
     {isPushdownVisible && (
       <Pushdown closeFunc={closePushdown}>
         {unviewedCoinCount > 0 && (
-          <Text
-            style={{
-              color: 'white',
-              fontFamily: vars.fontRegular,
-              fontSize: 18,
-            }}>
+          <Text style={styles.pushdownText}>
             You received {unviewedCoinCount} luvup{unviewedCoinCount > 1
               ? 's'
               : ''}!
           </Text>
         )}
         {unviewedJalapenoCount > 0 && (
-          <Text
-            style={{
-              color: 'white',
-              fontFamily: vars.fontRegular,
-              fontSize: 18,
-            }}>
+          <Text style={styles.pushdownText}>
             You received {unviewedJalapenoCount} jalapeno{unviewedJalapenoCount >
             1
               ? 's'
@@ -64,25 +45,10 @@ export default ({
         )}
       </Pushdown>
     )}
-    <View
-      style={{
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        alignSelf: 'stretch',
-        paddingLeft: 8,
-        paddingRight: 8,
-        paddingBottom: 8,
-      }}>
+    <View style={styles.navUiContainer}>
       {_.isNumber(coinCount) && _.isNumber(jalapenoCount) ? (
-        <TouchableOpacity
-          onPress={onScoreClick}
-          style={{
-            flex: 0.33,
-            flexDirection: 'row',
-            alignItems: 'center',
-          }}>
-          {unreadReceivedLoveNoteCount && (
+        <TouchableOpacity onPress={onScoreClick} style={styles.coinCountBtn}>
+          {unreadReceivedLoveNoteCount > 0 && (
             <NotificationDot
               style={{
                 left: -2,
@@ -95,61 +61,20 @@ export default ({
             fill={vars.blueGrey300}
             stroke={vars.blueGrey500}
           />
-          <Text
-            style={{
-              paddingLeft: 5,
-              fontFamily: vars.fontBlack,
-              fontSize: 16,
-              color: vars.blueGrey500,
-            }}>
-            {coinCount}
-          </Text>
+          <Text style={styles.coinCountText}>{coinCount}</Text>
         </TouchableOpacity>
       ) : (
-        <View
-          style={{
-            flex: 0.33,
-            flexDirection: 'row',
-            alignItems: 'center',
-          }}
-        />
+        <View style={styles.coinCountBtn} />
       )}
       <TouchableOpacity
         onPress={onRelationshipScoreClick}
-        style={{
-          flex: 0.33,
-          alignItems: 'center',
-        }}>
-        <Text
-          style={{
-            fontFamily: vars.fontBlack,
-            color: vars.blueGrey300,
-            fontSize: 12,
-          }}>
-          Relationship Score
-        </Text>
-        <Text
-          style={{
-            fontFamily: vars.fontBlack,
-            color: vars.blueGrey700,
-            fontSize: 30,
-          }}>
-          {relationshipScore}%
-        </Text>
+        style={styles.scoreBtn}>
+        <Text style={styles.scoreTitleText}>Relationship Score</Text>
+        <Text style={styles.scoreText}>{relationshipScore}%</Text>
       </TouchableOpacity>
       {_.isString(userFirstName) && userFirstName.length > 1 ? (
-        <TouchableOpacity
-          onPress={onInitialsClick}
-          style={{
-            flex: 0.33,
-            alignItems: 'flex-end',
-          }}>
-          <Text
-            style={{
-              fontFamily: vars.fontBlack,
-              fontSize: 16,
-              color: vars.blueGrey500,
-            }}>
+        <TouchableOpacity onPress={onInitialsClick} style={styles.menuBtn}>
+          <Text style={styles.menuText}>
             {userFirstName.substr(0, 1).toUpperCase()}
             {userLastName.substr(0, 1).toUpperCase()}
             {loverFirstName ? ' + ' : ''}
@@ -160,7 +85,7 @@ export default ({
           </Text>
         </TouchableOpacity>
       ) : (
-        <View style={{ flex: 33 }} />
+        <View style={styles.menuBtn} />
       )}
     </View>
   </View>
