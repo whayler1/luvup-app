eb589abf19718a3498c68bdf1ebdcb2992db08e1
Justin Worsdale <whayler1@gmail.com>
2018-11-05 21:40:56 -0500

    CreateQuizChoice answer placeholder

--- src/containers/CreateQuiz/CreateQuizChoice.js
+++ src/containers/CreateQuiz/CreateQuizChoice.js
@@ -5,6 +5,12 @@ import { TextInput } from 'react-native';
 // import styles from './CreateQuiz.styles';
 import { forms } from '../../styles';
 
+const placeholders = [
+  'Think of a good answer',
+  'Think of an answer',
+  'Think of a funny answer',
+];
+
 class CreateQuizChoice extends PureComponent {
   static propTypes = {
     index: PropTypes.number.isRequired,
@@ -12,6 +18,12 @@ class CreateQuizChoice extends PureComponent {
     enabled: PropTypes.bool,
     onChange: PropTypes.func.isRequired,
   };
+
+  constructor(props) {
+    super(props);
+    this.placeholder =
+      placeholders[Math.floor(Math.random() * placeholders.length)];
+  }
 
   handleChangeText = text => {
     this.props.onChange(text, this.props.index);
@@ -21,6 +33,7 @@ class CreateQuizChoice extends PureComponent {
     return (
       <TextInput
         style={forms.input}
+        placeholder={this.placeholder}
         onChangeText={this.handleChangeText}
         value={this.props.value}
         enabled={this.props.enabled}
