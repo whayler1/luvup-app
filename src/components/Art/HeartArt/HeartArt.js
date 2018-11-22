7d66c1d0c0e8616923c42b18067f7dcc86a86504
Justin Worsdale <whayler1@gmail.com>
2018-11-22 14:06:14 -0500

    add razzle dazzle timeline head bg and svg heart

--- src/components/Art/HeartArt/HeartArt.js
+++ src/components/Art/HeartArt/HeartArt.js
@@ -6,32 +6,40 @@ import { getAnimatedRelationshipScoreFill } from '../../../helpers/getRelationsh
 
 class HeartArt extends PureComponent {
   static propTypes = {
-    animatedFillPct: PropTypes.object.isRequired,
+    animatedFillPct: PropTypes.object,
     scale: PropTypes.number,
+    fill: PropTypes.string,
   };
 
   componentDidMount() {
     const { animatedFillPct } = this.props;
+    if (animatedFillPct) {
+      this.animatedListener = animatedFillPct.addListener(() => {
+        const fill = getAnimatedRelationshipScoreFill(animatedFillPct);
 
-    this.animatedListener = animatedFillPct.addListener(() => {
-      const fill = getAnimatedRelationshipScoreFill(animatedFillPct);
-
-      // Make sure that the ref was acquired.
-      if (this.pathRef) {
-        this.pathRef.setNativeProps({ fill: extractBrush(fill.__getValue()) });
-      }
-    });
+        // Make sure that the ref was acquired.
+        if (this.pathRef) {
+          this.pathRef.setNativeProps({
+            fill: extractBrush(fill.__getValue()),
+          });
+        }
+      });
+    }
   }
 
   componentWillUnmount() {
-    this.props.animatedFillPct.removeListener(this.animatedListener);
+    if (this.props.animatedFillPct) {
+      this.props.animatedFillPct.removeListener(this.animatedListener);
+    }
   }
 
   setPathRef = ref => (this.pathRef = ref);
 
   render() {
     const { animatedFillPct, scale } = this.props;
-    const fill = getAnimatedRelationshipScoreFill(animatedFillPct).__getValue();
+    const fill = animatedFillPct
+      ? getAnimatedRelationshipScoreFill(animatedFillPct).__getValue()
+      : this.props.fill;
 
     return (
       <Svg width={891 * scale} height={807 * scale}>
