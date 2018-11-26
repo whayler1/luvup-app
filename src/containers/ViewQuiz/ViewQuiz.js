29aaf20dcb362934926456ea8757ba45f4201a54
Justin Worsdale <whayler1@gmail.com>
2018-11-25 19:13:14 -0500

    ViewQuiz isSender property

--- src/containers/ViewQuiz/ViewQuiz.js
+++ src/containers/ViewQuiz/ViewQuiz.js
@@ -2,6 +2,7 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { View, ScrollView, Text } from 'react-native';
 import { connect } from 'react-redux';
+import _ from 'lodash';
 
 import QuizDisplay from '../../components/QuizDisplay';
 import { quiz as styles } from '../../styles';
@@ -10,12 +11,15 @@ class ViewQuiz extends PureComponent {
   static propTypes = {
     quizItemId: PropTypes.string.isRequired,
     quizItemDictionary: PropTypes.object.isRequired,
+    userId: PropTypes.string.isRequired,
+    loverFirstName: PropTypes.string.isRequired,
   };
 
   constructor(props) {
     super(props);
 
     this.quizItem = props.quizItemDictionary[props.quizItemId];
+    this.isSender = this.quizItem.senderId === props.userId;
 
     this.state = {
       recipientChoiceId: this.quizItem.recipientChoiceId,
@@ -24,13 +28,20 @@ class ViewQuiz extends PureComponent {
 
   render() {
     const {
-      state: { recipientChoiceId },
       quizItem,
+      isSender,
+      // state: { recipientChoiceId },
     } = this;
+    const loverFirstName = _.upperFirst(this.props.loverFirstName);
     return (
       <ScrollView
         style={styles.scrollContainer}
         contentContainerStyle={styles.scrollContent}>
+        {isSender ? (
+          <Text>You sent {loverFirstName} a quiz</Text>
+        ) : (
+          <Text>{loverFirstName} sent you a quiz</Text>
+        )}
         <QuizDisplay quizItem={quizItem} />
       </ScrollView>
     );
@@ -40,5 +51,5 @@ class ViewQuiz extends PureComponent {
 export default connect(state => ({
   quizItemDictionary: state.quizItem.quizItemDictionary,
   userId: state.user.id,
-  loverId: state.lover.id,
+  loverFirstName: state.lover.firstName,
 }))(ViewQuiz);
