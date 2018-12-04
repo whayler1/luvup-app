048f0048bd1873ebf78f6800fd429e4cc44ae6d5
Justin Worsdale <whayler1@gmail.com>
2018-12-03 20:09:59 -0500

    tests back to fetch error

--- babel.config.js
+++ babel.config.js
@@ -19,38 +19,40 @@ module.exports = {
     // 'module:metro-react-native-babel-preset',
     '@babel/preset-react',
   ],
-  test: {
-    presets: ['@babel/preset-env', '@babel/preset-react'],
-    plugins: [
-      '@babel/plugin-proposal-export-default-from',
-      '@babel/plugin-proposal-export-namespace-from',
-      '@babel/plugin-syntax-dynamic-import',
-      '@babel/plugin-syntax-import-meta',
-      '@babel/plugin-proposal-class-properties',
-      '@babel/plugin-proposal-json-strings',
-      [
-        '@babel/plugin-proposal-decorators',
-        {
-          legacy: true,
-        },
+  env: {
+    test: {
+      presets: ['@babel/preset-env', '@babel/preset-react'],
+      plugins: [
+        '@babel/plugin-proposal-export-default-from',
+        '@babel/plugin-proposal-export-namespace-from',
+        '@babel/plugin-syntax-dynamic-import',
+        '@babel/plugin-syntax-import-meta',
+        '@babel/plugin-proposal-class-properties',
+        '@babel/plugin-proposal-json-strings',
+        [
+          '@babel/plugin-proposal-decorators',
+          {
+            legacy: true,
+          },
+        ],
+        '@babel/plugin-proposal-function-sent',
+        '@babel/plugin-proposal-numeric-separator',
+        '@babel/plugin-proposal-throw-expressions',
+        '@babel/plugin-transform-flow-strip-types',
+        // ['@babel/plugin-proposal-decorators', { legacy: true }],
+        // ['@babel/plugin-proposal-class-properties', { loose: true }],
+        // '@babel/plugin-syntax-dynamic-import',
+        '@babel/plugin-transform-regenerator',
+        [
+          '@babel/plugin-transform-runtime',
+          {
+            helpers: false,
+            regenerator: true,
+          },
+        ],
       ],
-      '@babel/plugin-proposal-function-sent',
-      '@babel/plugin-proposal-numeric-separator',
-      '@babel/plugin-proposal-throw-expressions',
-      '@babel/plugin-transform-flow-strip-types',
-      // ['@babel/plugin-proposal-decorators', { legacy: true }],
-      // ['@babel/plugin-proposal-class-properties', { loose: true }],
-      // '@babel/plugin-syntax-dynamic-import',
-      '@babel/plugin-transform-regenerator',
-      [
-        '@babel/plugin-transform-runtime',
-        {
-          helpers: false,
-          regenerator: true,
-        },
-      ],
-    ],
-    // only: ['./**/*.js', 'node_modules/jest-runtime'],
+      only: ['./**/*.js', 'node_modules/jest-runtime'],
+    },
   },
 };
 
