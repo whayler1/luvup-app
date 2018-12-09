5a273860d171c7cef1150684f41a9bb8888927f1
Justin Worsdale <whayler1@gmail.com>
2018-12-09 10:38:09 -0500

    tweak babel config so tests pass again

--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,28 @@
 /* eslint-disable import/no-commonjs */
 module.exports = {
   plugins: [
-    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    '@babel/plugin-transform-flow-strip-types',
+    [
+      '@babel/plugin-proposal-decorators',
+      {
+        legacy: true,
+      },
+    ],
+    [
+      '@babel/plugin-proposal-class-properties',
+      {
+        loose: true,
+      },
+    ],
     '@babel/plugin-proposal-optional-chaining',
     '@babel/plugin-syntax-dynamic-import',
     '@babel/plugin-proposal-export-default-from',
     '@babel/plugin-proposal-export-namespace-from',
     '@babel/plugin-syntax-import-meta',
-    '@babel/plugin-proposal-class-properties',
     '@babel/plugin-proposal-json-strings',
     '@babel/plugin-proposal-function-sent',
     '@babel/plugin-proposal-numeric-separator',
     '@babel/plugin-proposal-throw-expressions',
-    '@babel/plugin-transform-flow-strip-types',
-    // ['@babel/plugin-proposal-decorators', { legacy: true }],
-    // ['@babel/plugin-proposal-class-properties', { loose: true }],
     '@babel/plugin-transform-regenerator',
     [
       '@babel/plugin-transform-runtime',
@@ -28,15 +36,10 @@ module.exports = {
     '@babel/preset-env',
     'module:metro-react-native-babel-preset',
     '@babel/preset-react',
-    'babel-preset-expo',
   ],
   env: {
     test: {
-      presets: [
-        '@babel/preset-env',
-        '@babel/preset-react',
-        'babel-preset-expo',
-      ],
+      presets: ['@babel/preset-env', '@babel/preset-react'],
       plugins: [
         '@babel/plugin-proposal-export-default-from',
         '@babel/plugin-proposal-export-namespace-from',
