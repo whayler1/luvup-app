a4bd60af2b43cefce75725faf5d19a980d900ea2
Justin Worsdale <whayler1@gmail.com>
2019-01-21 21:29:55 -0500

    login e2e tests pass

--- e2e/Login.spec.js
+++ e2e/Login.spec.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-commonjs */
 import { reloadApp } from 'detox-expo-helpers';
-import { generateUser, generateRelationship, login } from './helpers';
+import { generateRelationship, login } from './helpers';
 
 describe.only('login', () => {
   beforeEach(async () => {
@@ -9,41 +9,28 @@ describe.only('login', () => {
     });
   });
 
-  describe.only('When login is correct and user is in a relationship', () => {
-    let email;
-    let password;
-
-    beforeEach(async () => {
-      // console.log('');
-      // await generateRelationship();
-      // const user = await generateUser();
-      // email = user.email;
-      // password = user.password;
-    });
-
-    it.only('should go to dashboard and be ableto logout', async () => {
-      const { user, lover, requestLoverRes } = await generateRelationship();
-      // console.log('user', user);
-      // console.log('lover', lover);
-      // console.log('requestLoverRes', requestLoverRes);
+  describe('When login is correct and user is in a relationship', () => {
+    it('should go to dashboard and be ableto logout', async () => {
+      const { user } = await generateRelationship();
       await expect(element(by.id('login-title'))).toBeVisible();
-      // await login(email, password);
+      await login(user.email, user.password);
 
-      // await waitFor(element(by.id('relatioship-score-label')))
-      //   .toBeVisible()
-      //   .withTimeout(5000);
+      await waitFor(element(by.id('relatioship-score-label')))
+        .toBeVisible()
+        .withTimeout(5000);
 
-      // await element(by.id('dashboard-top-nav-menu-button')).tap();
-      // await element(by.id('menu-logout')).tap();
-      // await element(by.id('menu-logout')).tap();
-      // await waitFor(element(by.id('login-title')))
-      //   .toBeVisible()
-      //   .withTimeout(3000);
+      await element(by.id('dashboard-top-nav-menu-button')).tap();
+      await element(by.id('menu-logout')).tap();
+      await element(by.id('menu-logout')).tap();
+      await waitFor(element(by.id('login-title')))
+        .toBeVisible()
+        .withTimeout(3000);
     });
 
     it('should remain logged in on reload', async () => {
+      const { user } = await generateRelationship();
       await expect(element(by.id('login-title'))).toBeVisible();
-      await login();
+      await login(user.email, user.password);
 
       await waitFor(element(by.id('relatioship-score-label')))
         .toBeVisible()
