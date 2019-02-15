e220165e99849cddf6691d584a4bf40fec78df2d
Justin Worsdale <whayler1@gmail.com>
2019-02-14 23:16:38 -0500

    reset pword success view

--- src/containers/ForgotPassword/ForgotPassword.js
+++ src/containers/ForgotPassword/ForgotPassword.js
@@ -2,10 +2,13 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { View, KeyboardAvoidingView, Text } from 'react-native';
+import { Actions } from 'react-native-router-flux';
+import { Button } from 'react-native-elements';
 
 import styles from './ForgotPassword.styles';
-import { modal, scene } from '../../styles';
+import { modal, scene, buttons, forms } from '../../styles';
 import { sendNewPassword as sendNewPasswordAction } from '../../redux/user/user.actions';
+import ForgotPasswordForm from './ForgotPasswordForm';
 
 class ForgotPassword extends PureComponent {
   static propTypes = {
@@ -19,7 +22,20 @@ class ForgotPassword extends PureComponent {
 
     this.state = {
       email: '',
+      isSuccess: false,
     };
+  }
+
+  componentDidUpdate(prevProps) {
+    if (
+      prevProps.isSendNewPasswordInFlight &&
+      !this.props.isSendNewPasswordInFlight &&
+      this.props.sendNewPasswordError.length < 1
+    ) {
+      /* eslint-disable react/no-did-update-set-state */
+      this.setState({ isSuccess: true });
+      /* eslint-enable react/no-did-update-set-state */
+    }
   }
 
   handleEmailChange = email => {
@@ -30,7 +46,44 @@ class ForgotPassword extends PureComponent {
     this.props.sendNewPassword(this.state.email);
   };
 
+  handleGoToLogin = () => {
+    Actions.login();
+  };
+
   render() {
+    if (this.state.isSuccess) {
+      return (
+        <KeyboardAvoidingView>
+          <View style={scene.contentNoTop}>
+            <Text style={modal.title}>Password Reset Email Sent</Text>
+            <Text style={[modal.copy, styles.copy]}>
+              An email with a temporary password has been sent to{' '}
+              {this.state.email}. After you log in with your new password you
+              will be prompted to change it to a password of your liking.
+            </Text>
+            <View style={forms.formGroup}>
+              <Button
+                onPress={this.handleGoToLogin}
+                containerViewStyle={buttons.container}
+                buttonStyle={buttons.infoSkeletonButton}
+                textStyle={buttons.infoSkeletonText}
+                title="Login"
+              />
+            </View>
+          </View>
+        </KeyboardAvoidingView>
+      );
+    }
+    const {
+      props: {
+        isSendNewPasswordInFlight,
+        sendNewPasswordError,
+        sendNewPassword,
+      },
+      state: { email },
+      handleEmailChange,
+      handleSubmit,
+    } = this;
     return (
       <KeyboardAvoidingView>
         <View style={scene.contentNoTop}>
@@ -39,6 +92,14 @@ class ForgotPassword extends PureComponent {
             Enter your email address below and you will be emailed a temporary
             password.
           </Text>
+          <ForgotPasswordForm
+            isSendNewPasswordInFlight={isSendNewPasswordInFlight}
+            sendNewPasswordError={sendNewPasswordError}
+            sendNewPassword={sendNewPassword}
+            email={email}
+            onEmailChange={handleEmailChange}
+            onSubmit={handleSubmit}
+          />
         </View>
       </KeyboardAvoidingView>
     );
---
+++ src/containers/ForgotPassword/ForgotPasswordForm.js
@@ -0,0 +1,84 @@
+import React, { PureComponent } from 'react';
+import { View, Text, TextInput } from 'react-native';
+import PropTypes from 'prop-types';
+import { Button } from 'react-native-elements';
+
+import { vars, forms, buttons } from '../../styles';
+
+class ForgotPasswordForm extends PureComponent {
+  static propTypes = {
+    isSendNewPasswordInFlight: PropTypes.bool.isRequired,
+    sendNewPasswordError: PropTypes.string.isRequired,
+    onSubmit: PropTypes.func.isRequired,
+    onEmailChange: PropTypes.func.isRequired,
+    email: PropTypes.string.isRequired,
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isFocus: false,
+    };
+  }
+
+  handleBlur = () => {
+    this.setState({ isFocus: false });
+  };
+
+  handleFocus = () => {
+    this.setState({ isFocus: true });
+  };
+
+  render() {
+    const {
+      props: {
+        isSendNewPasswordInFlight,
+        sendNewPasswordError,
+        onSubmit,
+        onEmailChange,
+        email,
+      },
+      state: { isFocus },
+    } = this;
+
+    return (
+      <View>
+        <View style={forms.formGroup}>
+          <Text style={forms.label}>Email</Text>
+          <TextInput
+            style={[forms.input, isFocus && forms.inputFocus]}
+            onFocus={this.handleFocus}
+            onBlur={this.handleBlur}
+            onChangeText={onEmailChange}
+            value={email}
+            maxLength={50}
+            placeholder="jane.doe@email.com"
+            placeholderTextColor={vars.blueGrey100}
+            autoCapitalize={'none'}
+            editable={!isSendNewPasswordInFlight}
+            spellCheck={false}
+            keyboardType="email-address"
+            returnKeyType="go"
+            onSubmitEditing={onSubmit}
+          />
+          {sendNewPasswordError.length > 0 && (
+            <Text style={forms.error}>{sendNewPasswordError}</Text>
+          )}
+        </View>
+        <View style={forms.formGroup}>
+          <Button
+            onPress={onSubmit}
+            containerViewStyle={buttons.container}
+            buttonStyle={buttons.infoButton}
+            textStyle={buttons.infoText}
+            title={isSendNewPasswordInFlight ? 'Submitting…' : 'Submit'}
+            disabled={isSendNewPasswordInFlight}
+          />
+        </View>
+      </View>
+    );
+  }
+}
+
+export default ForgotPasswordForm;
