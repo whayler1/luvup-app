dac1caaf88ba2f40e30ebece0170b807b72d17b4
Justin Worsdale <whayler1@gmail.com>
2019-07-15 23:17:08 -0400

    add getUserInviteWithId to acceptInvite constructor

--- src/containers/AcceptInvite/AcceptInvite.js
+++ src/containers/AcceptInvite/AcceptInvite.js
@@ -1,25 +1,45 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { Text } from 'react-native';
+import { connect } from 'react-redux';
 
 import FormScene from '../../components/FormScene';
+import { getUserInviteWithId as getUserInviteWithIdAction } from '../../redux/userInvite/userInvite.actions';
 
 class AcceptInvite extends PureComponent {
   static propTypes = {
     userInviteId: PropTypes.string.isRequired,
+    // isGetUserInviteWithIdInFlight: PropTypes.bool.isRequired,
+    // getUserInviteWithIdError: PropTypes.string.isRequired,
+    getUserInviteWithId: PropTypes.func.isRequired,
   };
 
   constructor(props) {
-    // call user invite endpoint
+    super(props);
+    if (props.userInviteId) {
+      props.getUserInviteWithId(props.userInviteId);
+    }
   }
 
   render() {
+    const {
+      props: { userInviteId },
+    } = this;
     return (
       <FormScene>
-        <Text>Accept invite form!</Text>
+        <Text>Accept invite form! {userInviteId}</Text>
       </FormScene>
     );
   }
 }
 
-export default AcceptInvite;
+export default connect(
+  state => ({
+    isGetUserInviteWithIdInFlight:
+      state.userInvite.isGetUserInviteWithIdInFlight,
+    getUserInviteWithIdError: state.userInvite.getUserInviteWithIdError,
+  }),
+  {
+    getUserInviteWithId: getUserInviteWithIdAction,
+  }
+)(AcceptInvite);
