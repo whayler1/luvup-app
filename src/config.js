73ea7caa372451d53cb7c73ad0303e7d23408348
Justin Worsdale <whayler1@gmail.com>
2019-08-07 20:26:43 -0400

    debug forgot password spec

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
-// const baseUrl = 'http://localhost:3000';
+const baseUrl = 'http://localhost:3000';
 // const baseUrl = 'http://192.168.1.206:3000';
-const baseUrl = 'https://app.luvup.io';
+// const baseUrl = 'https://app.luvup.io';
+// const baseUrl = 'https://luvup-api.justinworsdale.now.sh';
 const segmentWriteKey =
   process.env.SEGMENT_WRITE_KEY || '8m6bF7b3J2aDf6t2sF5NLSmLhwpb0UVm';
 
