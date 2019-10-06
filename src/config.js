c918152140642ecc9759582c7142e8bed93d14ca
Justin Worsdale <whayler1@gmail.com>
2019-10-06 17:59:06 -0400

    bump version

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
-const baseUrl = 'http://localhost:3000/api';
+// const baseUrl = 'http://localhost:3000/api';
 // const baseUrl = 'http://192.168.1.206:3000';
-// const baseUrl = 'https://luvup.io/api';
+const baseUrl = 'https://luvup.io/api';
 // const baseUrl = 'https://luvup-api.justinworsdale.now.sh';
 const segmentWriteKey =
   process.env.SEGMENT_WRITE_KEY || '8m6bF7b3J2aDf6t2sF5NLSmLhwpb0UVm';
