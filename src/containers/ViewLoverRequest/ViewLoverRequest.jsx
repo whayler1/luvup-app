8b9fad32c1aad10f32575f3283de66c2f232d1d1
Justin Worsdale <whayler1@gmail.com>
2019-10-08 10:01:31 -0400

    style view lover request

--- src/containers/ViewLoverRequest/ViewLoverRequest.jsx
+++ src/containers/ViewLoverRequest/ViewLoverRequest.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 import distanceInWords from 'date-fns/distance_in_words';
 
 import Button, { BUTTON_STYLES } from '../../components/Button';
 import FormScene from '../../components/FormScene';
-import { scene } from '../../styles';
+import { scene, forms } from '../../styles';
 
 const ViewLoverRequest = ({ loverRequest: { createdAt, sender } }) => {
-  function handleSubmit() {
+  function handleDeny() {
+    //
+  }
+  function handleAccept() {
     //
   }
   return (
     <FormScene>
       <>
         <Text style={[scene.titleCopy, scene.textCenter]}>
-          You Received a Lover Request
+          Lover Request Received
         </Text>
         <Text style={[scene.bodyCopy, scene.textCenter, scene.gutterTop]}>
           {sender.firstName} {sender.lastName} ({sender.email}) sent you a Lover
@@ -23,8 +26,18 @@ const ViewLoverRequest = ({ loverRequest: { createdAt, sender } }) => {
             addSuffix: true,
           })}
         </Text>
-        <Button buttonStyles={BUTTON_STYLES.DANGER_SKELETON} title="Deny" />
-        <Button title="Accept" />
+        <View style={[forms.row, scene.gutterDoubleTop]}>
+          <View style={forms.buttonCell2ColLeft}>
+            <Button
+              buttonStyles={BUTTON_STYLES.DANGER_SKELETON}
+              title="Deny"
+              onPress={handleDeny}
+            />
+          </View>
+          <View style={forms.buttonCell2ColRight}>
+            <Button title="Accept" onPress={handleAccept} />
+          </View>
+        </View>
       </>
     </FormScene>
   );
