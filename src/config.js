1d266faac6a7c76da1a5dbfbedcbc3b029a7867f
Justin Worsdale <whayler1@gmail.com>
2019-10-27 12:50:06 +0100

    point to luvup api

--- src/config.js
+++ src/config.js
@@ -1,7 +1,6 @@
-const baseUrl = 'http://localhost:3000/api';
-// const baseUrl = 'http://192.168.1.206:3000';
-// const baseUrl = 'https://luvup.io/api';
-// const baseUrl = 'https://luvup-api.justinworsdale.now.sh';
+// const baseUrl = 'http://localhost:3000/api';
+const baseUrl = 'https://luvup.io/api';
+
 const segmentWriteKey =
   process.env.SEGMENT_WRITE_KEY || '8m6bF7b3J2aDf6t2sF5NLSmLhwpb0UVm';
 
