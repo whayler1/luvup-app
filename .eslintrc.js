4399fa248fc0e004d6f3894d0d86f09ee5af0302
Justin Worsdale <justin@elysiumhealth.com>
2019-11-22 09:13:57 -0500

    update eslintrc to play nice with vscode

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,50 +1,43 @@
 module.exports = {
-    "extends": [
-      "eslint:recommended",
-      "plugin:react/recommended",
-      "react-native",
-      "react-native-prettier"
-    ],
-    "plugins": [
-      "react",
-      "react-native",
-      "babel",
-      "import",
-      "prettier",
-    ],
-    parser: 'babel-eslint',
-    parserOptions: {
-      ecmaVersion: 9,
-      ecmaFeatures: {
-        jsx: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'react-native',
+    'react-native-prettier',
+  ],
+  plugins: ['react', 'react-native', 'babel', 'import', 'prettier'],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 9,
+    ecmaFeatures: {
+      jsx: true,
+    },
+    sourceType: 'module',
+  },
+  rules: {
+    'linebreak-style': ['error', 'unix'],
+    semi: ['error', 'always'],
+    'react-native/no-color-literals': 'warn',
+    'react-native/no-inline-styles': 'warn',
+    'react/jsx-handler-names': 'warn',
+    'no-invalid-this': 0,
+    'babel/no-invalid-this': 1,
+    'no-console': ['warn'],
+    'import/no-namespace': 0,
+    'prettier/prettier': [
+      'error',
+      {},
+      {
+        usePrettierrc: true,
       },
-      sourceType: 'module',
-    },
-    "rules": {
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ],
-        "react-native/no-color-literals": "warn",
-        "react-native/no-inline-styles": "warn",
-        "react/jsx-handler-names": "warn",
-        "no-invalid-this": 0,
-        "babel/no-invalid-this": 1,
-        "no-console": [
-          "warn"
-        ],
-        "import/no-namespace": 0,
-    },
-    "globals": {
-      "element": true,
-      "by": true,
-      "context": true,
-      "waitFor": true,
-      "device": true,
-      "__DEV__": true,
-    }
+    ],
+  },
+  globals: {
+    element: true,
+    by: true,
+    context: true,
+    waitFor: true,
+    device: true,
+    __DEV__: true,
+  },
 };
