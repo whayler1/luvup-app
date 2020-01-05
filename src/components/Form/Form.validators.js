66c09eb071f9072e4f9141f23f22bbad416dd556
Justin Worsdale <whayler1@gmail.com>
2020-01-05 09:55:02 -0500

    add password validator

--- src/components/Form/Form.validators.js
+++ src/components/Form/Form.validators.js
@@ -2,9 +2,16 @@ import { emailRegex } from '../../helpers';
 
 const FORM_VALIDATORS = {};
 
-const EMAIL_REGEX_VALIDATOR = value =>
+const EMAIL_REGEX_VALIDATOR = (value) =>
   emailRegex.test(value) ? '' : 'Please provide a valid email';
 
+const PASSWORD_REGEX_VALIDATOR = (value) =>
+  /^\S{8,}/.test(value)
+    ? ''
+    : 'Password must be at least 8 characters and can not contain empty spaces';
+
 FORM_VALIDATORS.EMAIL_VALIDATORS = [EMAIL_REGEX_VALIDATOR];
+
+FORM_VALIDATORS.PASSWORD_VALIDATORS = [PASSWORD_REGEX_VALIDATOR];
 
 export default FORM_VALIDATORS;
